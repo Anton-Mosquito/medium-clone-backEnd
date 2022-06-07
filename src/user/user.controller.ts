4aaf5a93daa9fa995f7d6a3045f9114113540c4e
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-06-07 16:28:30 +0300

    create user dto, import to conroller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
 
 @Controller()
@@ -6,7 +7,11 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(): Promise<any> {
-    return this.userService.createUser();
+  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+    console.log(
+      '🚀 ~ file: user.controller.ts ~ line 10 ~ UserController ~ createUser ~ createUserDto',
+      createUserDto,
+    );
+    return this.userService.createUser(createUserDto);
   }
 }
