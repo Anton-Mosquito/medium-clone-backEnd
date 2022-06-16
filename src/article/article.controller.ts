f5ab88e6a6b27b2193ac6376bec766be9e9cfd71
Anton Komarnytskyi <anton.komarnytskyi@masterofcode.com>
2022-06-16 16:34:11 +0300

    improve create article in controller, connect articles with user, create service for item into database, article entity and migration

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Post } from '@nestjs/common';
+import { User } from '@app/user/decorators/user.decorator';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { UserEntity } from '@app/user/user.entity';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ArticleService } from './article.service';
+import { CreateArticleDto } from './dto/cteateArticle.dto';
 
 @Controller('articles')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
-  async create() {
-    const article = await this.articleService.createArticle();
+  @UseGuards(AuthGuard)
+  async create(
+    @User() currentUser: UserEntity,
+    @Body('article') createArticleDto: CreateArticleDto,
+  ): Promise<any> {
+    const article = await this.articleService.createArticle(
+      currentUser,
+      createArticleDto,
+    );
     return article;
   }
 }
